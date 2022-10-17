a7d81c066613eeac1abf3fec21bd864b795b1a6a
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-17 13:21:29 +0300

    DevTools Aktif etmek

---
+++ src/main/java/com/mesutcalim/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.mesutcalim.utils;
+
+public class ConfigProgramming {
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically
+    //Advenced Settings ==Allow Auto-make to start even (Clicked)
+}
