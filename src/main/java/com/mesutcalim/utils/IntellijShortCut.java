d430c28e202e04cedf0847fb71cc1be1139e206e
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-17 13:20:52 +0300

    kisayollar

---
+++ src/main/java/com/mesutcalim/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.mesutcalim.utils;
+
+public class IntellijShortCut {
+/*    Settings ==> ctrl + alt + s
+    Alt + Ins ==> constructor, toString, getter, setter
+    Ctrl + / ==> single comment
+    Ctrl + shift + / ==> multiple comment */
+}
