c2633509419a1aeb8fc446ff779b834827808d96
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-18 00:36:11 +0300

    @PostConstructor

---
+++ src/main/java/com/mesutcalim/bean/BeanConfig.java
@@ -0,0 +1,16 @@
+package com.mesutcalim.bean;
+
+import com.mesutcalim.dto.BeanDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BeanConfig {
+    @Bean(initMethod = "initialBeanMethod44", destroyMethod = "destroyBeanMethod44")
+    @Scope("singleton") //request session
+    public BeanDto beanDto(){
+        return BeanDto.builder().id(0L).beanName("bean adı").beanData("bean data").build();
+
+    }
+}
---
+++ src/main/java/com/mesutcalim/controller/BeanController.java
@@ -0,0 +1,25 @@
+package com.mesutcalim.controller;
+
+import com.mesutcalim.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class BeanController {
+
+    @Autowired
+    BeanConfig beanConfig;
+
+
+    // http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto")
+    @ResponseBody // html olmadan ekranda göster
+    public String getBeanDto(){
+        return beanConfig.beanDto()+ ".";
+    }
+}
