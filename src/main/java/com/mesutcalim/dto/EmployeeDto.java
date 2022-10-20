563facc5f24b9fecdde30f48dc269cba83d1f41e
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-20 14:12:35 +0300

    EmployeeDto

---
+++ src/main/java/com/mesutcalim/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package com.mesutcalim.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+//DTO: Data Transfer Object
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+public class EmployeeDto {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String emailId;
+}
