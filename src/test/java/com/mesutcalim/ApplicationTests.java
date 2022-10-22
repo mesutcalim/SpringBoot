38f8da0cb1a91e5129782f7db2af439aab63a139
mesutcalim <mesutcalim.eee@gmail.com>
2022-10-23 00:52:52 +0300

    Unit Test (CRUD)

--- src/test/java/com/mesutcalim/ApplicationTests.java
+++ src/test/java/com/mesutcalim/ApplicationTests.java
@@ -1,13 +1,68 @@
 package com.mesutcalim;
 
+import com.mesutcalim.data.entity.EmployeeEntity;
+import com.mesutcalim.data.repository.EmployeeRepository;
+import com.mesutcalim.test.TestCrud;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class ApplicationTests {
+class ApplicationTests implements TestCrud {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    @Override
+    public void testCreate() {
+        EmployeeEntity employeeEntity = EmployeeEntity.builder()
+                .firstName("Mesut Test").lastName("Çalım Test")
+                .emailId("mesutcalim@gmail.com")
+                .build();
+        employeeRepository.save(employeeEntity);
+        // nesne null ise assertion hatasını göndersin
+        assertNotNull(employeeRepository.findById(1L).get());
+    }
+
+    @Test
+    @Override
+    public void testList() {
+        List<EmployeeEntity> list = employeeRepository.findAll();
+        assertThat(list).size().isGreaterThan(0);
+    }
+
+    @Test
+    @Override
+    public void testFindById() {
+        EmployeeEntity employeeEntity = employeeRepository.findById(1L).get();
+        assertEquals("Mesut Test", employeeEntity.getFirstName());
+    }
+
+    @Test
+    @Override
+    public void testUpdate() {
+        EmployeeEntity employeeEntity = employeeRepository.findById(1L).get();
+        employeeEntity.setFirstName("Mesut 51 Test");
+        employeeRepository.save(employeeEntity);
+
+        assertNotEquals("Mesut Test", employeeRepository.findById(1L).get().getFirstName());
+
+    }
+
+    @Test
+    @Override
+    public void testDelete() {
+        employeeRepository.deleteById(1L);
+        assertThat(employeeRepository.existsById(1L)).isFalse();
+    }
 }
---
+++ src/test/java/com/mesutcalim/test/TestCrud.java
@@ -0,0 +1,16 @@
+package com.mesutcalim.test;
+
+public interface TestCrud {
+    //Test Crud
+    public void testCreate();
+
+    public void testUpdate();
+
+    public void testList();
+
+    public void testFindById();
+
+    public void testDelete();
+
+
+}
